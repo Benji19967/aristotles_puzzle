f8aa785f8593bae0838f6b2af1d39b1ef6decf7d
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-12-05 23:21:49 -0500

    Update rust solution, needs lots of cleanup

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,10 @@ fn main() {
 
 type Board = Vec<Vec<u32>>;
 
+fn hashset(data: &[u32]) -> HashSet<u32> {
+    HashSet::from_iter(data.iter().cloned())
+}
+
 fn get_valid_board_combinations() -> () {
     let rows_of_3 = get_all_rows_summing_to_38(3);
     let rows_of_4 = get_all_rows_summing_to_38(4);
@@ -36,21 +40,75 @@ fn get_valid_board_combinations() -> () {
 
     let mut using: HashSet<u32> = HashSet::new();
     let mut board_combinations: Vec<Vec<&Vec<u32>>> = vec![];
+    let mut num_iterations = 0;
 
     for x in rows_of_3.iter().combinations(2) {
         using.extend(x[0]);
+        if !using.is_disjoint(&hashset(&x[1])) {
+            for i in x[0].iter() {
+                using.remove(i);
+            }
+            continue;
+        }
+        using.extend(x[1]);
         for y in rows_of_4.iter().combinations(2) {
+            if !using.is_disjoint(&hashset(&y[0])) {
+                continue;
+            }
+            using.extend(y[0]);
+            if !using.is_disjoint(&hashset(&y[1])) {
+                for i in y[0].iter() {
+                    using.remove(i);
+                }
+                continue;
+            }
+            using.extend(y[1]);
             for z in rows_of_5.iter() {
+                if !using.is_disjoint(&hashset(&z)) {
+                    continue;
+                }
                 // println!("{:?}", x);
                 // println!("{:?}", y);
                 // println!("{:?}", z);
-                let mut board: Vec<&Vec<u32>> = vec![];
-                board.push(x[0]);
-                board.push(y[0]);
-                board.push(z);
-                board.push(y[1]);
-                board.push(x[1]);
+
+                // println!("{:?}", using);
+                for row_1 in x[0].iter().permutations(x[0].len()) {
+                    for row_2 in y[0].iter().permutations(y[0].len()) {
+                        for row_3 in z.iter().permutations(z.len()) {
+                            for row_4 in y[1].iter().permutations(y[1].len()) {
+                                for row_5 in x[1].iter().permutations(x[1].len()) {
+                                    let mut board: Vec<&Vec<&u32>> = vec![];
+                                    board.push(&row_1);
+                                    board.push(&row_2);
+                                    board.push(&row_3);
+                                    board.push(&row_4);
+                                    board.push(&row_5);
+                                    num_iterations += 1;
+                                    // println!("{:?}", board);
+                                    if num_iterations % 1000000 == 0 {
+                                        println!("{}", num_iterations);
+                                    }
+                                    if is_board_valid(board) {
+                                        panic!("Found solution");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
+            for i in y[0].iter() {
+                using.remove(i);
+            }
+            for i in y[1].iter() {
+                using.remove(i);
+            }
+        }
+        for i in x[0].iter() {
+            using.remove(i);
+        }
+        for i in x[1].iter() {
+            using.remove(i);
         }
     }
 }
@@ -64,4 +122,27 @@ fn get_all_rows_summing_to_38(row_length: usize) -> Vec<Vec<u32>> {
         }
     }
     rows
+}
+
+fn is_board_valid(board: Vec<&Vec<&u32>>) -> bool {
+    for line in DIAGONAL_LINE_DOWN_LEFT_INDEXES.iter() {
+        let mut sum_of_line = 0;
+        for x_y in line.iter() {
+            sum_of_line += board[x_y[0]][x_y[1]];
+        }
+        if sum_of_line != 38 {
+            return false;
+        }
+    }
+    for line in DIAGONAL_LINE_DOWN_RIGHT_INDEXES.iter() {
+        let mut sum_of_line = 0;
+        for x_y in line.iter() {
+            sum_of_line += board[x_y[0]][x_y[1]];
+        }
+        if sum_of_line != 38 {
+            return false;
+        }
+    }
+    println!("{:?}", board);
+    true
 }
